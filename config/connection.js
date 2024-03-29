d968e80297d387dad046e2357f4ce6691270a9b1
tonyG <tx.tony9@gmail.com>
2024-03-29 07:54:19 -0700

    added new connection to config folder

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/socialmedia');
+mongoose.connect('mongodb+srv://txtony9:<password>@snapi.2iej5rm.mongodb.net/');
 
 module.exports = mongoose.connection;
